2775a2e4d0720237334f64949adc1b7b30c5bca5
Андрей Макаров <tyuiu.MakarovAY@outlook.com>
2025-10-12 20:17:54 +0500

    Task1

---
+++ Tyuiu.MakarovAY.Sprint2.Task1.V6.Test/DataServiceTest.cs
@@ -0,0 +1,20 @@
+﻿using Tyuiu.MakarovAY.Sprint2.Task1.V6.Lib;
+namespace Tyuiu.MakarovAY.Sprint2.Task1.V6.Test
+
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidGetLogicOperations()
+        {
+            DataService ds = new DataService();
+            int a = 915; int b = 169; int c = 174; int d = 133;
+            bool[] res = new bool[6];
+            res = ds.GetLogicOperations(a, b, c, d);
+            bool[] wait = new bool[6] { false, false, true, false, true, false };
+            CollectionAssert.AreEqual(wait, res);
+
+        }
+    }
+}
