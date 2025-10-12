a151a6bfa588749239f4c5cdc1d529dde967069b
Андрей Макаров <tyuiu.MakarovAY@outlook.com>
2025-10-12 19:22:59 +0500

    Add project files.

---
+++ Tyuiu.MakarovAY.Sprint2.Task0.V28.Lib/DataService.cs
@@ -0,0 +1,19 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint2;
+namespace Tyuiu.MakarovAY.Sprint2.Task0.V28.Lib
+{
+    public class DataService : ISprint2Task0V28
+    {
+        public bool[] GetCompareOperations(int x, int y)
+        {
+            bool[] res = new bool[6];
+            res[0] = x == y;
+            res[1] = x * 735 != y * 111;
+            res[2] = x * 7 < y;
+            res[3] = x > y;
+            res[4] = x * 7 <= y;
+            res[5] = x >= y;
+
+            return res;
+        }
+    }
+}
---
+++ Tyuiu.MakarovAY.Sprint2.Task0.V28.Test/DataServiceTest.cs
@@ -0,0 +1,20 @@
+﻿using Tyuiu.MakarovAY.Sprint2.Task0.V28.Lib;
+namespace Tyuiu.MakarovAY.Sprint2.Task0.V28.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidGetCompareOperations()
+        {
+            DataService ds = new DataService();
+            int x = 111; int y = 735;
+            bool[] res = new bool[6];
+            res = ds.GetCompareOperations(x, y);
+            bool[] wait = new bool[6] {false, false, false, false, false, false};
+
+            CollectionAssert.AreEqual(wait, res);
+
+        }
+    }
+}
