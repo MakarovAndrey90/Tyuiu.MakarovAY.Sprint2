3d71d938c0539ff8715e1593793b1ed48e99f15d
Андрей Макаров <tyuiu.MakarovAY@outlook.com>
2025-10-14 14:07:13 +0500

    Task3

---
+++ Tyuiu.MakarovAY.Sprint2.Task3.V8.Test/DataServiceTest.cs
@@ -0,0 +1,44 @@
+﻿using System.Security.Cryptography.X509Certificates;
+using Tyuiu.MakarovAY.Sprint2.Task3.V8.Lib;
+namespace Tyuiu.MakarovAY.Sprint2.Task3.V8.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidCalc1()
+        {
+            DataService ds = new DataService();
+            double x = 1;
+            double wait = -4.588;
+            Assert.AreEqual(wait, ds.Calculate(x));
+        }
+
+        [TestMethod]
+        public void ValidCalc2()
+        {
+            DataService ds = new DataService();
+            double x = 0;
+            double wait = 0.75;
+            Assert.AreEqual(wait, ds.Calculate(x));
+        }
+
+        [TestMethod]
+        public void ValidCalc3()
+        {
+            DataService ds = new DataService();
+            double x = -1;
+            double wait = 0.25;
+            Assert.AreEqual(wait, ds.Calculate(x));
+        }
+
+        [TestMethod]
+        public void ValidCalc4()
+        {
+            DataService ds = new DataService();
+            double x = -13;
+            double wait = -142.615;
+            Assert.AreEqual(wait, ds.Calculate(x));
+        }
+    }
+}
---
+++ Tyuiu.MakarovAY.Sprint2.Task3.V8/Program.cs
@@ -0,0 +1,34 @@
+﻿using Tyuiu.MakarovAY.Sprint2.Task3.V8.Lib;
+namespace Tyuiu.MakarovAY.Sprint2.Task3.V8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #2 | Выполнил: Макаров А. Я. | РППБ-25-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #2                                                               *");
+            Console.WriteLine("* Тема: Вложенные операторы if - else                                     *");
+            Console.WriteLine("* Задание #3                                                              *");
+            Console.WriteLine("* Вариант #8                                                              *");
+            Console.WriteLine("* Выполнил: Макаров А. Я. | РППБ-25-1                                     *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу, которая вычисляет требуемое значение функции Y      *");
+            Console.WriteLine("* с использованием вложенных оператор if-else, где пользователь вводит    *");
+            Console.WriteLine("* значение переменной X с клавиатуры. Округлить до 3х знаков после запятой*");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            double x;
+
+            Console.WriteLine("Введите значение переменной x: ");
+            x = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Значение функции равно: " + ds.Calculate(x));
+
+        }
+    }
+}
