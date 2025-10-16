9c03d8171a7a7c0f0e55e777ae6e43b10eb21634
Андрей Макаров <tyuiu.MakarovAY@outlook.com>
2025-10-16 19:17:45 +0500

    Task7

---
+++ Tyuiu.MakarovAY.Sprint2.Task7.V15.Test/DataServiceTest.cs
@@ -0,0 +1,17 @@
+﻿using Tyuiu.MakarovAY.Sprint2.Task7.V15.Lib;
+namespace Tyuiu.MakarovAY.Sprint2.Task7.V15.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidCheckDotInShadedArea()
+        {
+            DataService ds = new DataService();
+            double x = 1; double y = 1;
+            bool res = ds.CheckDotInShadedArea(x, y);
+            bool wait = true;
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
